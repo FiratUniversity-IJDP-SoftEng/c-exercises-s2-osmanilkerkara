2be764dc13d6688583323fd749280a31b9863451
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2024-11-12 15:11:13 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    printf("Numbers divisible by 3 from 100 to 50:\n");
+    for(int i = 100; i >= 50; i--) {
+        if(i % 3 == 0) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
